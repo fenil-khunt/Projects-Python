70bf3e2c1ab96f2514e426bc30765fb2a90bf344
Fenil Khunt <97497591+fenil-khunt@users.noreply.github.com>
2022-05-06 00:30:16 +0530

    05/05/22

--- Eye_Tracker.py
+++ Eye_Tracker.py
@@ -28,3 +28,53 @@ def contouring(thresh, mid, img ,right=False):
     except:
             
         pass
+        
+detector = dlib.get_frontal_face_detector()
+predictor = dlib.shap_predictor('shape_68.det')
+
+left = [36, 37, 38, 39, 40, 41]
+right =[42, 43, 44, 45, 46, 47]
+
+cap = cv2.ViedoCapture(0)
+ret, img = cap.read()
+thresh = img.copy()
+
+cv2.namedWindow('image')
+kernal =np.ones((9, 9), np.unit8)
+
+def nothing(x):
+    pass
+
+cv2.createTrackbar('threshold', 'image', 0, 255, nothing)
+
+while(true):
+    ret, img = cap.read()
+    gray =cv2.cvtColor(img, cv2.COLOR_BDR2GRAY)
+    rects = detector(gray, 1)
+    for rect in rects:
+        shape = predictor(gray, rect)
+        shape = shap_to_np(shape)
+        mask = np.zeros(img.shape[:2], dtype=np.uint8)
+        mask = eye_on_mask(mask, right)
+        mask = eye_on_mask(mask, left)
+        mask = cv2.dilate(mask. kernel, 5)
+        eyes = bitwise_and(img, img, mask=mask)
+        eyes[mask] = [255, 255, 255]
+        mid = (shape[42][0] + shape[39][0]) // 2
+        eyes_gray = cv2.cvtColor(eyes, cv2.COLORBGR2GRAY)
+        threshold = cv2.grtTrackerbarPos('threshold', 'image')
+        _, thresh = cv2.threshold(eyes_gray, 'threshold', 255, cv2.THRESH_BINARY)
+        thresh = cv2.erode(thresh, None, intretions=2)
+        thresh = cv2.dilate(thresh, None, intretions=4)
+        thresh = cv2.medianBlur(thresh, 3)
+        thresh = cv2.bitwise_not(thresh)
+        contouring(threshe[:, 0:mid], mid, img)
+        contouring(threshe[:, mid:], mid, img, True)
+    
+    cv2.imshow('eyes', img)
+    cv2.imshow("image", thresh)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break 
+        
+cap.release()
+cv2.destroyAllWindows()
